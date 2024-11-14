4e949db76ccacd3b41cb0c0007105a5489d0fc3b
Varma558 <varmatg393@gmail.com>
2024-11-14 07:45:51 +0530

    Add files via upload

---
+++ Training Feed/string functions.py
@@ -0,0 +1,10 @@
+# string functions
+word = " pyt hon dg e ger g "
+word = word.lower()
+word = word.upper()
+word = word.lower()
+word = word.capitalize()
+word = word.replace(" ", "")
+word = word.count("g")
+# word = word.replace(" ", "")
+print(word)
